daca74863c1b84f2cbf447b8b5a0a5686463f04a
Ben Gowers <benjamingowers@gmail.com>
2026-07-02 12:04:25 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false
+
+        const sortedS = [...s].sort()
+        const sortedT = [...t].sort()
+
+        for (let i = 0; i < s.length; i++) {
+            if (sortedS[i] !== sortedT[i]) return false
+        }
+
+        return true
+    }
+}
